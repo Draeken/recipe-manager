9d98bbab2af11ca022d910258843f1b2eda3fb59
Draeken <pbm764927@gmail.com>
2021-02-01 15:31:07 +0100

    feat: change new input to array form

--- src/components/creation/StepCreation.tsx
+++ src/components/creation/StepCreation.tsx
@@ -1,41 +1,96 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { Field, Form, Formik } from 'formik';
-import { Box, makeStyles, Paper } from '@material-ui/core';
+import { Button, IconButton, makeStyles, Paper, TextField } from '@material-ui/core';
+import { DeleteSharp } from '@material-ui/icons';
+import { Field, FieldArray, Form, Formik } from 'formik';
 
 const styles = makeStyles((theme) => ({
   root: {
     margin: theme.spacing(2, 0),
     padding: theme.spacing(1),
     '& .MuiTextField-root': {
-      paddingBottom: theme.spacing(2),
+      paddingRight: theme.spacing(2),
     },
+  },
+  smallField: {
+    width: '100px',
   },
 }));
 
 const StepCreation = () => {
-  const router = useRouter();
   const classes = styles();
-  const { query } = router;
   return (
     <Paper className={classes.root}>
-      <Box m={2}>
-        <Formik
-          initialValues={{
-            name: '',
-            family: '',
-          }}
-          onSubmit={(...a) => console.log('submit', a)}
-        >
-          <Form>
-            <Field name="name" placeholder="Name" />
-            <Field name="family" placeholder="Family" />
-            <Link href={`${query.id}/step/${1}`}>
-              <button>ADD STEP</button>
-            </Link>
+      <Formik
+        initialValues={{
+          inputs: [
+            {
+              family: '',
+              details: '',
+              quantity: 1,
+              unit: '',
+            },
+          ],
+        }}
+        onSubmit={(...a) => console.log('submit', a)}
+        render={({ values }) => (
+          <Form autoComplete="off">
+            <FieldArray
+              name="inputs"
+              render={(arrayHelpers) => (
+                <>
+                  {values.inputs.map((_, index) => (
+                    <div>
+                      <Field
+                        name={`inputs.${index}.family`}
+                        label="Input"
+                        as={TextField}
+                        variant="filled"
+                      />
+                      <Field
+                        name={`inputs.${index}.details`}
+                        label="Additional details"
+                        as={TextField}
+                        variant="filled"
+                      />
+                      <Field
+                        className={classes.smallField}
+                        type="number"
+                        name={`inputs.${index}.quantity`}
+                        label="Quantity"
+                        as={TextField}
+                        variant="filled"
+                      />
+                      <Field
+                        className={classes.smallField}
+                        name={`inputs.${index}.unit`}
+                        label="Unit"
+                        as={TextField}
+                        variant="filled"
+                      />
+                      <IconButton aria-label="delete" onClick={() => arrayHelpers.remove(index)}>
+                        <DeleteSharp />
+                      </IconButton>
+                    </div>
+                  ))}
+                  <br />
+                  <Button
+                    color="primary"
+                    onClick={() =>
+                      arrayHelpers.push({
+                        family: '',
+                        details: '',
+                        quantity: 1,
+                        unit: '',
+                      })
+                    }
+                  >
+                    add input
+                  </Button>
+                </>
+              )}
+            />
           </Form>
-        </Formik>
-      </Box>
+        )}
+      />
     </Paper>
   );
 };
